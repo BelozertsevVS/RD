1ba1ff5e4285e9f6c8e39dd09353678dc994ef80
BelozertsevVS <bvs.wl1980@gmail.com>
2023-04-17 19:01:16 +0300

    Lesson#25.Exceptions.Part1

---
+++ src/com/ua/robot/lesson25/NegativeNumberException.java
@@ -0,0 +1,11 @@
+package com.ua.robot.lesson25;
+
+public class NegativeNumberException extends Exception {
+    public NegativeNumberException(String message) {
+        super(message);
+    }
+
+    public NegativeNumberException() {
+
+    }
+}
